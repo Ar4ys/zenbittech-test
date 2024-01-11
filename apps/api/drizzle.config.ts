91eefc2d3647c1b46b704e4cec206c4e87ec2aec
Ar4ys <ar4ys@pm.me>
2024-01-11 01:00:04 +0100

    Fixed drizzle.config.ts

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,16 +1,25 @@
-import 'dotenv/config';
+import path from 'path';
 
+import * as dotenv from 'dotenv';
 import type { Config } from 'drizzle-kit';
+
+import { environmentValidator, getEnvironmentFilePath } from './environment';
+
+for (const envFile of getEnvironmentFilePath()) {
+  dotenv.config({ path: path.resolve(__dirname, envFile) });
+}
+
+const env = environmentValidator(process.env);
 
 export default {
   schema: './db/schema.ts',
   out: './db/migrations',
   driver: 'pg',
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE!,
+    host: env.DB_HOST,
+    port: env.DB_PORT,
+    user: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
+    database: env.DB_DATABASE,
   },
 } satisfies Config;
